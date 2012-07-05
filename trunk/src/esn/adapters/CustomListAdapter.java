f8399bf8314c205fe7ca3203fe80dc6476e10139
lnbienit@gmail.com <lnbienit@gmail.com@6a5d87a1-9760-71f7-0fc2-a17f2a145e53>
2012-07-05 06:46:44 +0000

    --Bo sung CustomListAdapter

---
+++ trunk/src/esn/adapters/CustomListAdapter.java
@@ -0,0 +1,152 @@
+package esn.adapters;
+
+import java.util.ArrayList;
+
+import esn.classes.ImageLoader;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public abstract class CustomListAdapter<T> extends BaseAdapter {
+	private Activity activity;
+	private ArrayList<T> data;
+	private static LayoutInflater inflater = null;
+	private int layoutRow;
+	private ImageLoader imageLoader;
+	private int idIcon;
+
+	public CustomListAdapter(Activity activity, ArrayList<T> list, int layoutRow, int idIcon) {
+		this.activity = activity;
+		this.data = list;
+		this.layoutRow = layoutRow;
+		list = null;
+		this.idIcon = idIcon;
+		inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);		
+		this.imageLoader = new ImageLoader(activity.getApplicationContext());
+		this.imageLoader.setDefaultEmptyImage(this.idIcon);
+	}
+	
+	public CustomListAdapter(Activity activity, ArrayList<T> list, int layoutRow) {
+		this.activity = activity;
+		this.data = list;
+		list = null;
+		this.layoutRow = layoutRow;
+		inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);		
+	}
+	
+	public void add(ArrayList<T> list){
+		int len = list.size();
+		for(int i = 0; i < len; i++){
+			data.add(list.get(i));
+		}
+		list = null;
+		this.notifyDataSetChanged();
+	}
+	
+	public void add(T item){
+		data.add(item);
+		item = null;
+		this.notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return data.size();
+	}
+
+	@Override
+	public Object getItem(int index) {
+		return data.get(index);
+	}
+
+	@Override
+	public long getItemId(int index) {
+		return index;
+	}
+
+	@Override
+	public View getView(int index, View convertView, ViewGroup parent) {
+		View vi = convertView;
+		Object bean = data.get(index);
+		final Object holder;
+		if (convertView == null){
+			vi = inflater.inflate(layoutRow, null);
+			holder = createHolder(vi);
+			vi.setTag(holder);
+		}else{
+			holder = vi.getTag();
+		}
+		customRowView(bean, holder);
+		return vi;
+	}
+	
+	public void setDefaultEmptyImage(int idIcon){
+		if(imageLoader == null){
+			this.imageLoader = new ImageLoader(activity.getApplicationContext());
+		}
+		this.idIcon = idIcon;
+		this.imageLoader.setDefaultEmptyImage(this.idIcon);
+	}
+	
+	protected abstract void customRowView(Object rowBean, Object rowHolder);
+
+	protected abstract Object createHolder(View convertView);
+	
+	public void displayImage(String src, ImageView image){
+		if(imageLoader == null){
+			this.imageLoader = new ImageLoader(activity.getApplicationContext());
+		}
+		imageLoader.displayImage(src, activity, image);
+	}
+	
+	public Activity getActivity() {
+		return activity;
+	}
+
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+	}
+	
+	public void stopThread(){
+		if(imageLoader != null)
+			this.imageLoader.stopThread();
+	}
+	
+	public void clearCache(){
+		if(imageLoader != null)
+			this.imageLoader.clearCache();
+	}
+
+	public ArrayList<T> getData() {
+		return data;
+	}
+
+	public void setData(ArrayList<T> data) {
+		this.data = data;
+		this.notifyDataSetChanged();
+	}
+
+	public ImageLoader getImageLoader() {
+		return imageLoader;
+	}
+
+	public void setImageLoader(ImageLoader imageLoader) {
+		this.imageLoader = imageLoader;
+	}
+
+	public void setLayoutRow(int layoutRow) {
+		this.layoutRow = layoutRow;
+	}
+
+	public int getLayoutRow() {
+		return layoutRow;
+	}
+
+	public int getDefaultEmptyImage() {
+		return idIcon;
+	}
+}
--- trunk/src/esn/adapters/ListViewFriendsAdapter.java
+++ trunk/src/esn/adapters/ListViewFriendsAdapter.java
@@ -1,81 +1,39 @@
 package esn.adapters;
 
 import java.util.ArrayList;
-
 import esn.activities.R;
-import esn.classes.ImageLoader;
 import esn.models.FriendsListsDTO;
 import android.app.Activity;
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ListViewFriendsAdapter extends BaseAdapter {
-	private Activity activity;
-	private ArrayList<FriendsListsDTO> data;
-	private static LayoutInflater inflater = null;
-	public ImageLoader imageLoader;
-	
+public class ListViewFriendsAdapter extends CustomListAdapter<FriendsListsDTO> {
 	private class ViewHolder {
 		public TextView title;
 		public TextView disc;
 		public ImageView image;
 	}
-
-	public ListViewFriendsAdapter(Activity a, ArrayList<FriendsListsDTO> listFrd) {
-		this.activity = a;
-		this.data = listFrd;
-		listFrd = null;
-		inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		this.imageLoader = new ImageLoader(activity.getApplicationContext());
-		this.imageLoader.setDefaultEmptyImage(R.drawable.ic_no_avata);
-	}
 	
-	public void add(ArrayList<FriendsListsDTO> listFrd){
-		for (FriendsListsDTO frd : listFrd) {
-			data.add(frd);
-		}
-		listFrd = null;
+	public ListViewFriendsAdapter(Activity activity, ArrayList<FriendsListsDTO> listFrd) {
+		super(activity, listFrd, R.layout.listitem_row, R.drawable.ic_no_avata);
 	}
 
 	@Override
-	public int getCount() {
-		return data.size();
-	}
-
-	@Override
-	public Object getItem(int index) {
-		return data.get(index);
-	}
-
-	@Override
-	public long getItemId(int index) {
-		return index;
-	}
-
-	@Override
-	public View getView(int index, View convertView, ViewGroup parent) {
-		View vi = convertView;
-		final ViewHolder holder;
-		if (convertView == null){
-			vi = inflater.inflate(R.layout.listitem_row, null);
-			holder = new ViewHolder();
-			holder.title = (TextView) vi.findViewById(R.id.txtViewTitle);
-			holder.disc = (TextView) vi.findViewById(R.id.txtViewDescription);
-			holder.image = (ImageView) vi.findViewById(R.id.imgViewLogo);
-			vi.setTag(holder);
-		}else{
-			holder = (ViewHolder) vi.getTag();
-		}
-
-		FriendsListsDTO bean = data.get(index);
+	protected void customRowView(Object rowBean, Object rowHolder) {
+		ViewHolder holder = (ViewHolder) rowHolder;
+		FriendsListsDTO bean = (FriendsListsDTO) rowBean;
 		holder.title.setText(bean.Name);
 		holder.disc.setText("Phone: " + bean.Phone);//So ban chung
-		imageLoader.displayImage(bean.Avatar, activity, holder.image);
-		return vi;
+		displayImage(bean.Avatar, holder.image);
+	}
+
+	@Override
+	protected Object createHolder(View convertView) {
+		ViewHolder holder = new ViewHolder();
+		holder.title = (TextView) convertView.findViewById(R.id.txtViewTitle);
+		holder.disc = (TextView) convertView.findViewById(R.id.txtViewDescription);
+		holder.image = (ImageView) convertView.findViewById(R.id.imgViewLogo);
+		return holder;
 	}
 }
